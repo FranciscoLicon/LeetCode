d5727f403e9546bfe8ea97b9a5d7d324c152f2d3
FranciscoLicon <francisco17@ciencias.unam.mx>
2021-02-05 23:54:01 -0600

    Modificando métodos

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -2,13 +2,13 @@
  * @param {number} x
  * @return {boolean}
  */
+
+const reverseInt = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num);
+
 const isPalindrome = (x) => {
     if(x < 0){
         return false;
     }   
     return x === reverseInt(x);
 }
-
-const reverseInt = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num);
-
 console.log(isPalindrome(121)); //true
